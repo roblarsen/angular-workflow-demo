fee9f0907607971144531977a9bdb0f07ff65037
Rob Larsen <rob@htmlcssjavascript.com>
2021-05-26 10:09:53 -0400

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -25,12 +25,20 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-workflow-demo'),
+      dir: require('path').join(__dirname, './coverage/<project-name>'),
       subdir: '.',
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
-      ]
+      ],
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
+        }
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
